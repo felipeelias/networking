e93e30a22a648d8aac512a0fbf8a146f52e8ac9d
Felipe Elias Philipp <felipeelias@gmail.com>
2017-01-22 15:09:58 +0100

    Remove connection boilerplate with TCPServer

--- chat.rb
+++ chat.rb
@@ -4,30 +4,7 @@ require 'logger'
 include Socket::Constants
 
 logger = Logger.new(STDOUT)
-
-addresses = Addrinfo.getaddrinfo(nil, 3000, AF_INET, :STREAM, nil, AI_PASSIVE)
-
-server = nil
-addresses.each do |address|
-  server = Socket.new(AF_INET, SOCK_STREAM, 0)
-  logger.info("Starting server: #{server}")
-
-  begin
-    server.setsockopt(:SOCKET, :REUSEADDR, true)
-    if server.bind(address) < 0
-      logger.warn("can't bind #{socket} to #{address.inspect}, closing")
-      server.close
-    end
-  rescue => error
-    logger.error("#{error}: #{address.inspect}")
-    server.close if server
-  end
-end
-
-if server && !server.closed?
-  logger.debug("Listening...")
-  server.listen(10)
-end
+server = TCPServer.new(3000)
 
 clients = [server]
 loop do
@@ -36,22 +13,18 @@ loop do
   logger.debug("Received #select, #{read}")
 
   read.each do |read|
-    logger.debug("Processing #{read}")
-
     if read == server
-      client, address = server.accept
-      logger.info("New connection from #{client}:#{address.inspect}")
+      client = server.accept
       clients << client
-      logger.info("Total clients now: #{clients}")
+      logger.info("New connection from #{client}. Clients: #{clients.inspect}")
     else
-      logger.debug("Handling client #{read}")
       data = read.gets
+      logger.debug("Client sent: #{data.inspect}")
 
-      logger.debug("Client sent #{data.inspect}")
       if data.nil?
-        logger.debug("No data, closing connection for #{read}")
         clients.delete(read)
         read.close
+        logger.debug("No data, closing connection for #{read}")
       else
         clients.each do |client|
           if client != server && client != read
